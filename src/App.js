deab33ce775d3b1c77df7590524d329278aba503
sammartorana <sam.martorana@objectpartners.com>
2019-12-03 14:40:54 -0600

    converting to typescript

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+class ResumeForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      employeeName: '',
+      resumeName: '',
+      summary: '',
+      resumeSkills: Array(1).fill(null),
+      experiences: Array(1).fill(null),
+      education: Array(1).fill(null),
+      certifications: Array(1).fill(null),
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    const target = event.target.name;
+
+    this.setState({
+      [target]: event.target.value
+    });
+
+    console.log(this.state)
+  }
+
+  handleSubmit(event) {
+    console.log('Employee Name:', this.state.employeeName, "Summary:", this.state.summary);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Summary:
+          <textarea
+            name="summary"
+            onChange={this.handleChange} />
+        </label>
+        <br />
+        <label>
+          employeeName:
+          <input
+            name="employeeName"
+            type="text"
+            value={this.state.employeeName}
+            onChange={this.handleChange} />
+        </label>
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ResumeForm/>
     </div>
   );
 }
